51513aba2fab08b331276567e62235d50147980b
Vetrivel M.D <44906792+Vetrivel-Hari@users.noreply.github.com>
2022-07-12 22:52:49 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function setCookie(){
 	console.log(name);
 	console.log(rollno);
 
-	document.cookie="name="+name;
-	document.cookie="rollno="+rollno;
+	document.cookie="name=" + name + "; expires=Thu, 1 Dec 2022 12:00:00 UTC";
+	document.cookie="rollno="+rollno  + "; expires=Thu, 1 Dec 2022 12:00:00 UTC";;
 	
 	console.log(document.cookie);
 }
